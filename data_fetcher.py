0598f61261a4988153671dbf7e5cfbcd3bfe5650
abhiiticlass-star <abhiiticlass@gmail.com>
2026-06-09 09:47:58 +0530

    Create data_fetcher.py

---
+++ data_fetcher.py
@@ -0,0 +1,106 @@
+import requests
+import pandas as pd
+
+from config import (
+    API_KEY,
+    CANDLE_LIMIT
+)
+
+BASE_URL = (
+    "https://api.twelvedata.com/time_series"
+)
+
+
+def fetch_candles(
+    symbol,
+    interval
+):
+
+    params = {
+        "symbol": symbol,
+        "interval": interval,
+        "outputsize": CANDLE_LIMIT,
+        "apikey": API_KEY,
+        "format": "JSON"
+    }
+
+    response = requests.get(
+        BASE_URL,
+        params=params,
+        timeout=20
+    )
+
+    response.raise_for_status()
+
+    data = response.json()
+
+    if "values" not in data:
+
+        raise Exception(
+            f"Invalid API Response: {data}"
+        )
+
+    df = pd.DataFrame(
+        data["values"]
+    )
+
+    df = df.rename(
+        columns={
+            "datetime": "time"
+        }
+    )
+
+    numeric_columns = [
+        "open",
+        "high",
+        "low",
+        "close"
+    ]
+
+    for col in numeric_columns:
+
+        df[col] = pd.to_numeric(
+            df[col],
+            errors="coerce"
+        )
+
+    df = df.sort_values(
+        "time"
+    )
+
+    df.reset_index(
+        drop=True,
+        inplace=True
+    )
+
+    return df
+
+
+def get_market_data(
+    pair,
+    timeframe
+):
+
+    current_df = fetch_candles(
+        pair,
+        timeframe
+    )
+
+    if timeframe == "1min":
+
+        higher_df = fetch_candles(
+            pair,
+            "5min"
+        )
+
+    else:
+
+        higher_df = fetch_candles(
+            pair,
+            "5min"
+        )
+
+    return {
+        "current": current_df,
+        "higher": higher_df
+    }
