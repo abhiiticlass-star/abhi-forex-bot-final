7970f67b935bd45cc43673d5f46a2e8b48b00498
abhiiticlass-star <abhiiticlass@gmail.com>
2026-06-11 01:26:55 +0530

    Update trade_logger.py

--- trade_logger.py
+++ trade_logger.py
@@ -5,7 +5,12 @@ from datetime import datetime
 LOG_FILE = "data/signals_history.json"
 
 
-def log_signal(data):
+def log_signal(
+    data,
+    pair,
+    timeframe,
+    entry_price
+):
 
     if not os.path.exists(LOG_FILE):
 
